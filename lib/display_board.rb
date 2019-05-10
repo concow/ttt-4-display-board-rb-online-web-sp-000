6e99ec8c8df5e8452b5b0aa03c0a204bb7a316b4
Connor Cowan <ccowan003@comcast.net>
2019-05-10 22:26:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,72 +1,10 @@
-# Define display_board that accepts a board and prints
-# out the current state.
-
 def display_board(board)
+separator = "|"
+lines = "-----------"
 
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-
-  board = [" ", " ", " ", " ", " X ", " ", " ", " ", " "]
-
-
-  puts "   |   |   "
-  puts "-----------"
-  puts "   | #{board[4]}  |   "
-  puts "-----------"
-  puts "   |    |  "
-
-board = [" O ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-
-board = [" O ", " ", " ", " ", " X ", " ", " ", " ", " "]
-
-  puts "  #{board[0]} |   |   "
-  puts "-----------"
-  puts "   | #{board[4]}  |   "
-  puts "-----------"
-  puts "   |   |   "
-
-board = [" X", " X ", " X ", " ", "  ", " ", " ", " ", " "]
-
-  puts "  #{board[0]} | #{board[1]}  | #{board[2]}  "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-
-
-board = [" ", " ", " ", " ", "", " ", " O ", " O ", " O "]
-
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "  #{board[6]} | #{board[7]}  | #{board[8]}  "
-
-board = [" X ", " ", " ", " ", " X ", " ", " ", " ", " X "]
-
-  puts "  #{board[0]} |   |   "
-  puts "-----------"
-  puts "   | #{board[4]}  |   "
-  puts "-----------"
-  puts "   |   | #{board[8]}  "
-
-board = [" ", " ", " O ", " ", " O ", " ", " O ", " ", " "]
-
-  puts "   |   | #{board[2]}  "
-  puts "-----------"
-  puts "   | #{board[4]}  |   "
-  puts "-----------"
-  puts " #{board[6]}  |   |   "
-
+puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+puts "#{lines}"
+puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+puts "#{lines}"
+puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
 end
