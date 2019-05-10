065984349163d4558c5491e93c75308293a86c4c
Connor Cowan <ccowan003@comcast.net>
2019-05-10 22:26:20 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,72 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+  board = [" ", " ", " ", " ", " X ", " ", " ", " ", " "]
+
+
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | #{board[4]}  |   "
+  puts "-----------"
+  puts "   |    |  "
+
+board = [" O ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+board = [" O ", " ", " ", " ", " X ", " ", " ", " ", " "]
+
+  puts "  #{board[0]} |   |   "
+  puts "-----------"
+  puts "   | #{board[4]}  |   "
+  puts "-----------"
+  puts "   |   |   "
+
+board = [" X", " X ", " X ", " ", "  ", " ", " ", " ", " "]
+
+  puts "  #{board[0]} | #{board[1]}  | #{board[2]}  "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+
+board = [" ", " ", " ", " ", "", " ", " O ", " O ", " O "]
+
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "  #{board[6]} | #{board[7]}  | #{board[8]}  "
+
+board = [" X ", " ", " ", " ", " X ", " ", " ", " ", " X "]
+
+  puts "  #{board[0]} |   |   "
+  puts "-----------"
+  puts "   | #{board[4]}  |   "
+  puts "-----------"
+  puts "   |   | #{board[8]}  "
+
+board = [" ", " ", " O ", " ", " O ", " ", " O ", " ", " "]
+
+  puts "   |   | #{board[2]}  "
+  puts "-----------"
+  puts "   | #{board[4]}  |   "
+  puts "-----------"
+  puts " #{board[6]}  |   |   "
+
+end
