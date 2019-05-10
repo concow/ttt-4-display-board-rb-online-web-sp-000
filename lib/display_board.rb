5f39638a61a79a97a6571f858fbb3965f9a7804a
Connor Cowan <ccowan003@comcast.net>
2019-05-10 22:30:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,4 +7,10 @@ puts "#{lines}"
 puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
 puts "#{lines}"
 puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
+
+puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
+puts "-----------"
+puts " #{board[3]}  | #{board[4]}  | #{board[5]}  "
+puts "-----------"
+puts " #{board[6]}  | #{board[7]}  | #{board[8]}  "
 end
